aa9934a1608714e4a0209e151608c7987919d10e
Alan Jordan <alanpjordan@gmail.com>
2017-05-26 11:02:46 +1200

    fake date

---
+++ client/components/Tile.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default class Tile extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className='tile'>
+        
+      </div>
+    )
+  }
+
+}
