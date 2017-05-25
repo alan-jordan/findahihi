4076ee57cf4271a4862eccd2ef7f3f81e121abae
Alan Jordan <alanpjordan@gmail.com>
2017-05-26 10:31:27 +1200

    Header working, tests passing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 
 import * as api from '../api'
+import Header from './Header'
 
 export default class App extends React.Component {
   render () {
--- test/client/app.test.js
+++ test/client/app.test.js
@@ -6,3 +6,15 @@ import * as sinon from 'sinon'
 import './setup-dom'
 
 import App from '../../client/components/App'
+
+App.prototype.componentDidMount = () => {}
+
+test('Container class exists', t => {
+  const wrapper = mount(<App />)
+  t.is(wrapper.find('.container').exists(), true)
+})
+
+test('Logo class in nav bar contains find a hihi', t => {
+  const wrapper = mount(<App />)
+  t.is(wrapper.find('.logo a').text(), 'find a hihi')
+})
